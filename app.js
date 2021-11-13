52672aeeeb10469aedbefeecc2ab173b45c67a93
Yanaphat Sarantraivich <81007074+yanaphats@users.noreply.github.com>
2021-11-13 16:27:22 +0700

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -21,9 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
 
 // error handler
 app.use(function(err, req, res, next) {
